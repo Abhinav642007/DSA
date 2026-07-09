75237c7ebb2e50bd941f1855775d633cff6de859
Abhinav Gupta <justavi.3289@gmail.com>
2026-07-09 12:51:16 +0530

    Time: 0 ms (100%), Space: 12.2 MB (88.67%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def plusOne(self, arr):
+        carry = 1
+
+        for i in range(len(arr) - 1, -1, -1):
+
+            total = arr[i] + carry
+
+            arr[i] = total % 10
+
+            carry = total // 10
+
+        if carry:
+            arr.insert(0, carry)
+
+        return arr
