26d817c10acbf06d85b51e0edfee27db2daf8f0c
Abhinav Gupta <justavi.3289@gmail.com>
2026-08-05 13:44:49 +0530

    Time: 0 ms (100%), Space: 16.3 MB (64.88%) - LeetHub

---
+++ 0061-rotate-list/0061-rotate-list.cpp
@@ -0,0 +1,57 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* findNthNode(ListNode* temp, int k) {
+        int cnt = 1;
+
+        while (temp != nullptr) {
+            if (cnt == k) return temp;
+            cnt++;
+            temp = temp->next;
+        }
+
+        return temp;
+    }
+
+    ListNode* rotateRight(ListNode* head, int k) {
+        if (head == nullptr || head->next == nullptr || k == 0)
+            return head;
+
+        ListNode* tail = head;
+        int len = 1;
+
+        // Find the length and tail of the linked list
+        while (tail->next != nullptr) {
+            tail = tail->next;
+            len++;
+        }
+
+        // Reduce unnecessary rotations
+        k %= len;
+        if (k == 0)
+            return head;
+
+        // Make the linked list circular
+        tail->next = head;
+
+        // Find the new tail
+        ListNode* newLastNode = findNthNode(head, len - k);
+
+        // The next node becomes the new head
+        head = newLastNode->next;
+
+        // Break the circle
+        newLastNode->next = nullptr;
+
+        return head;
+    }
+};
