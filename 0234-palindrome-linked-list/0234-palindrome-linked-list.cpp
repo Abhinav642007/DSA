9ebc8db6cbdf1d01639333bf09a9b8d59d017349
Abhinav Gupta <justavi.3289@gmail.com>
2026-08-10 15:05:12 +0530

    Time: 0 ms (100%), Space: 122 MB (37.6%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -13,48 +13,98 @@ public:
 
     // Reverse function
     ListNode* reverse(ListNode* head){
+    //     ListNode* prev = NULL;
+    //     ListNode* curr = head;
+
+    //     while(curr != NULL){
+    //         ListNode* next = curr->next;
+    //         curr->next = prev;
+    //         prev = curr;
+    //         curr = next;
+    //     }
+
+    //     return prev;
+    // }
+
+    // bool isPalindrome(ListNode* head) {
+
+    //     // Empty list ya single node
+    //     if(head == NULL || head->next == NULL)
+    //         return true;
+
+    //     // Step 1: Find middle
+    //     ListNode* slow = head;
+    //     ListNode* fast = head;
+
+    //     while(fast->next->next != NULL && fast->next != NULL){
+    //         slow = slow->next;
+    //         fast = fast->next->next;
+    //     }
+
+    //     // // Step 2: Reverse second half
+    //     // ListNode* second = reverse(slow);
+
+    //     // // Step 3: Compare
+    //     // ListNode* first = head;
+
+    //     // while(second != NULL){
+    //     //     if(first->val != second->val)
+    //     //         return false;
+
+    //     //     first = first->next;
+    //     //     second = second->next;
+    //     // }
+
+    //     // return true;
+    //     ListNode* newhead = reverse(slow->next);
+    //     ListNode* first = head;
+    //     ListNode* second = newhead;
+
+    //     while(second!=NULL){
+    //         if(first->val!=second->val){
+    //             reverse(newhead);
+    //             return false;
+    //         }
+    //         first=first->next;
+    //         second= second->next;
+    //     }
+    //     reverse(newhead);
+    //     return true;
         ListNode* prev = NULL;
         ListNode* curr = head;
 
-        while(curr != NULL){
+        while(curr!=NULL){
             ListNode* next = curr->next;
             curr->next = prev;
             prev = curr;
             curr = next;
         }
-
         return prev;
+
     }
-
-    bool isPalindrome(ListNode* head) {
-
-        // Empty list ya single node
-        if(head == NULL || head->next == NULL)
-            return true;
-
-        // Step 1: Find middle
+    bool isPalindrome(ListNode* head){
         ListNode* slow = head;
         ListNode* fast = head;
 
-        while(fast != NULL && fast->next != NULL){
+        while(fast->next!=NULL && fast->next->next!=NULL){
             slow = slow->next;
             fast = fast->next->next;
         }
+        ListNode* newhead = reverse(slow->next);
+        ListNode* first=head;
+        ListNode* second= newhead;
 
-        // Step 2: Reverse second half
-        ListNode* second = reverse(slow);
-
-        // Step 3: Compare
-        ListNode* first = head;
-
-        while(second != NULL){
-            if(first->val != second->val)
+        while(second!=NULL){
+            if(first->val!=second->val){
+                reverse(newhead);
                 return false;
 
-            first = first->next;
-            second = second->next;
+            }
+            first=first->next;
+            second=second->next;
         }
-
+        reverse(newhead);
         return true;
+
     }
 };
