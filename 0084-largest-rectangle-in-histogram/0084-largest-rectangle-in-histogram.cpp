0d5a31862862b1ccb689f6090b574b0e234738f0
Abhinav Gupta <justavi.3289@gmail.com>
2026-08-21 17:51:03 +0530

    Time: 23 ms (53.76%), Space: 85.5 MB (49.81%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -1,65 +1,146 @@
+// class Solution {
+// public:
+//     int largestRectangleArea(vector<int>& heights) {
+
+//         int n = heights.size();
+
+//         // Stack stores indices of bars
+//         // in increasing height order
+//         stack<int> st;
+
+//         int maxArea = 0;
+
+//         // Go one extra step with height 0
+//         // This forces us to process all remaining bars
+//         for (int i = 0; i <= n; i++) {
+
+//             // If i == n, assume current height = 0
+//           x  // Otherwise take the actual height
+//             int currentHeight = (i == n) ? 0 : heights[i];
+
+//             // If current bar is smaller than stack top,
+//             // the stack-top bar cannot extend further right
+//             while (!st.empty() &&
+//                    heights[st.top()] > currentHeight) {
+
+//                 // This bar is now being removed
+//                 int index = st.top();
+//                 st.pop();
+
+//                 // Height of the rectangle
+//                 int height = heights[index];
+
+//                 // i = Next Smaller Element (NSE)
+
+//                 int width;
+
+//                 if (st.empty()) {
+
+//                     // No Previous Smaller Element (PSE)
+//                     // So rectangle extends from index 0 to i-1
+//                     width = i;
+
+//                 } else {
+
+//                     // st.top() = Previous Smaller Element (PSE)
+//                     //
+//                     // i = Next Smaller Element (NSE)
+//                     //
+//                     // Therefore:
+//                     // width = NSE - PSE - 1
+//                     width = i - st.top() - 1;
+//                 }
+
+//                 // Rectangle area
+//                 int area = height * width;
+
+//                 // Keep the largest area
+//                 maxArea = max(maxArea, area);
+//             }
+
+//             // Store current index for future comparisons
+//             st.push(i);
+//         }
+
+//         return maxArea;
+//     }
+// };
 class Solution {
 public:
     int largestRectangleArea(vector<int>& heights) {
 
         int n = heights.size();
 
-        // Stack stores indices of bars
-        // in increasing height order
+        vector<int> prev(n);
+        vector<int> next(n);
+
         stack<int> st;
+
+        // -------------------------------
+        // 1. Previous Smaller Element
+        // -------------------------------
+
+        for (int i = 0; i < n; i++) {
+
+            // Remove greater/equal heights
+            while (!st.empty() && heights[st.top()] >= heights[i]) {
+                st.pop();
+            }
+
+            // If stack is empty, no smaller element on left
+            if (st.empty())
+                prev[i] = -1;
+            else
+                prev[i] = st.top();
+
+            // Store INDEX
+            st.push(i);
+        }
+
+
+        // Clear stack for next calculation
+        while (!st.empty())
+            st.pop();
+
+
+        // -------------------------------
+        // 2. Next Smaller Element
+        // -------------------------------
+
+        for (int i = n - 1; i >= 0; i--) {
+
+            // Remove greater/equal heights
+            while (!st.empty() && heights[st.top()] >= heights[i]) {
+                st.pop();
+            }
+
+            // If stack is empty, no smaller element on right
+            if (st.empty())
+                next[i] = n;
+            else
+                next[i] = st.top();
+
+            // Store INDEX
+            st.push(i);
+        }
+
+
+        // -------------------------------
+        // 3. Calculate Maximum Area
+        // -------------------------------
 
         int maxArea = 0;
 
-        // Go one extra step with height 0
-        // This forces us to process all remaining bars
-        for (int i = 0; i <= n; i++) {
+        for (int i = 0; i < n; i++) {
 
-            // If i == n, assume current height = 0
-            // Otherwise take the actual height
-            int currentHeight = (i == n) ? 0 : heights[i];
+            // Width between previous smaller and next smaller
+            int width = next[i] - prev[i] - 1;
 
-            // If current bar is smaller than stack top,
-            // the stack-top bar cannot extend further right
-            while (!st.empty() &&
-                   heights[st.top()] > currentHeight) {
+            // Area = height × width
+            int area = heights[i] * width;
 
-                // This bar is now being removed
-                int index = st.top();
-                st.pop();
-
-                // Height of the rectangle
-                int height = heights[index];
-
-                // i = Next Smaller Element (NSE)
-
-                int width;
-
-                if (st.empty()) {
-
-                    // No Previous Smaller Element (PSE)
-                    // So rectangle extends from index 0 to i-1
-                    width = i;
-
-                } else {
-
-                    // st.top() = Previous Smaller Element (PSE)
-                    //
-                    // i = Next Smaller Element (NSE)
-                    //
-                    // Therefore:
-                    // width = NSE - PSE - 1
-                    width = i - st.top() - 1;
-                }
-
-                // Rectangle area
-                int area = height * width;
-
-                // Keep the largest area
-                maxArea = max(maxArea, area);
-            }
-
-            // Store current index for future comparisons
-            st.push(i);
+            // Update maximum
+            maxArea = max(maxArea, area);
         }
 
         return maxArea;
