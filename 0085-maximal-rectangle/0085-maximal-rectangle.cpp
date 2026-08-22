a4bb90895dc1e8e033b4ace2052c60f19f9a4d31
Abhinav Gupta <justavi.3289@gmail.com>
2026-08-22 14:58:29 +0530

    Time: 3 ms (90.93%), Space: 19.2 MB (76.13%) - LeetHub

--- 0085-maximal-rectangle/0085-maximal-rectangle.cpp
+++ 0085-maximal-rectangle/0085-maximal-rectangle.cpp
@@ -1,108 +1,43 @@
 class Solution {
 public:
-
-    // LeetCode 84:
-    // Given histogram heights, find the largest rectangle area
-    int largestRectangleArea(vector<int>& heights) {
-
-        stack<int> st;   // Stack mein indices store karenge
-        int maxArea = 0;
-
-        // Extra 0 add kar rahe hain taaki
-        // last ke remaining bars bhi process ho jayein
-        heights.push_back(0);
-
-        for (int i = 0; i < heights.size(); i++) {
-
-            // Agar current height stack ke top se chhoti hai,
-            // toh stack ke top wale bar ka rectangle complete ho gaya
-            while (!st.empty() && heights[st.top()] > heights[i]) {
-
-                int height = heights[st.top()];
+    int largestrectangle(vector<int>& heights){
+        int n=heights.size();
+        stack<int> st;
+        int ans=0;
+        for(int i=0;i<=n;i++){
+            int curr=(i==n?0:heights[i]);
+            while(!st.empty() && heights[st.top()]>curr){
+                int h=heights[st.top()];
                 st.pop();
-
-                // Rectangle ki width calculate karo
                 int width;
-
-                if (st.empty()) {
-                    // Agar stack empty hai,
-                    // rectangle index 0 se start hua tha
-                    width = i;
-                }
-                else {
-                    // Stack ke top ke baad se
-                    // current index se pehle tak rectangle hai
-                    width = i - st.top() - 1;
-                }
-
-                // Rectangle area
-                int area = height * width;
-
-                // Maximum area update karo
-                maxArea = max(maxArea, area);
+                if(st.empty()) width=i;
+                else{
+                    width=i-st.top()-1;
+                }    ans=max(ans,h*width);
+                
+                
             }
-
-            // Current index ko stack mein daalo
             st.push(i);
         }
-
-        // Jo 0 add kiya tha usko remove kar do
-        heights.pop_back();
-
-        return maxArea;
+        return ans;
     }
-
-
-    // LeetCode 85:
-    // Find maximum rectangle containing only 1s
     int maximalRectangle(vector<vector<char>>& matrix) {
-
-        // Agar matrix empty hai
-        if (matrix.empty())
-            return 0;
-
-        // Number of rows
+        if(matrix.empty()) return 0;
         int rows = matrix.size();
-
-        // Number of columns
         int cols = matrix[0].size();
-
-        // Har column ki current height
-        vector<int> heights(cols, 0);
-
-        // Final maximum answer
-        int answer = 0;
-
-
-        // Har row ko process karo
-        for (int i = 0; i < rows; i++) {
-
-            // Current row ke har column ko check karo
-            for (int j = 0; j < cols; j++) {
-
-                if (matrix[i][j] == '1') {
-
-                    // Agar 1 hai,
-                    // previous height mein 1 add karo
-                    heights[j]++;
-
+        vector<int> height(cols,0);
+        int ans=0;
+        for(int i=0;i<rows;i++){
+            for(int j=0;j<cols;j++){
+                if(matrix[i][j]=='1'){
+                    height[j]++;
                 }
-                else {
-
-                    // Agar 0 hai,
-                    // continuous 1s ka chain toot gaya
-                    heights[j] = 0;
+                else{
+                    height[j]=0;
                 }
             }
-
-            // Current heights ko histogram maan kar
-            // LeetCode 84 solve karo
-            int currentArea = largestRectangleArea(heights);
-
-            // Overall maximum update karo
-            answer = max(answer, currentArea);
+            ans=max(ans,largestrectangle(height));
         }
-
-        return answer;
+        return ans;
     }
 };
